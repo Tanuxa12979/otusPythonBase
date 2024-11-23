26cdd0302f5ed7df47aa00efa5477492e24cf7cf
”Tatiana <tanuxa12979@mail.ru>
2024-11-23 12:41:13 +0300

    added docstrings

--- homework_02/controller.py
+++ homework_02/controller.py
@@ -4,6 +4,14 @@ import view
 import model
 import text_ru
 
+
+class FieldNotFound(Exception):
+    pass
+
+class MenuIsNotValid(Exception):
+    pass
+
+
 class Controller:
     def __init__(self):
         self.is_file_opened = False
@@ -11,11 +19,11 @@ class Controller:
         self.phonebook = model.PhoneBook()
 
     def open_file(self, file: model.FileWork) -> None:
-        '''
+        """
         открытие файла и запись списка конактов в phonebook
         :param file: файловый объект
         :return: None
-        '''
+        """
         if self.is_file_opened:
             view.print_msg(text_ru.FILE_IS_OPEN_ALREADY)
             return None
@@ -35,17 +43,23 @@ class Controller:
                 self.is_file_opened = True
 
     def write_to_file(self, file: model.FileWork) -> None:
-        '''
+        """
         Сохранение информации в файл
         :param file: файловый объект
         :return: None
-        '''
+        """
         file.write_to_file({'users': [value for key, value in self.phonebook.contacts.items()]})
         self.is_info_saved = True
         view.print_msg(text_ru.file_saved_info(file.path))
 
     @staticmethod
     def _file_check_and_add(file: model.FileWork, user_path: str) -> bool:
+        """
+        Проверка существования файла и его открытие
+        :param file: файловый объект
+        :param user_path: путь к к файлу
+        :return: true в случае успешного открытия, false иначе
+        """
         if user_path == '':
             view.print_msg(text_ru.successfully_chose_file(file.path))
             return True
@@ -62,7 +76,11 @@ class Controller:
             view.print_msg(text_ru.successfully_chose_file(file.path))
             return True
 
-    def add_contact_to_phonebook(self):
+    def add_contact_to_phonebook(self) -> bool:
+        """
+        выбор константа для добавления
+        :return: true в случае успешного добавления контакта
+        """
         contact_fields = view.add_contact_ask_fields()
         try:
             new_contact = self.phonebook.add_contact(contact_fields)
@@ -74,12 +92,21 @@ class Controller:
             return True
 
     def before_exit(self, file: model.FileWork) -> None:
+        """
+        запрос на сохранение перед завершением работы программы при наличии несохраненных данных
+        :param file: файловый объект
+        :return: none
+        """
         if not self.is_info_saved:
             save = view.user_input(text_ru.SAVE_BEFORE_EXIT)
             if save == 'save':
                 self.write_to_file(file)
 
     def delete_contact(self) -> bool:
+        """
+        Удаление константа
+        :return: true в случае успешного удаления
+        """
         id_contact = view.user_input(text_ru.DELETE_ID_USER)
         try:
             self.phonebook.delete_contact_by_id(int(id_contact))
@@ -91,7 +118,11 @@ class Controller:
             view.print_msg(text_ru.contact_deleted_successfully(id_contact))
             return True
 
-    def find_contact(self):
+    def find_contact(self) -> None:
+        """
+        Поиск и удаление контакта при его наличии
+        :return:
+        """
         text = view.user_input(text_ru.FIND_CONTACT_TEXT)
         found_contact_dict = self.phonebook.find_contact(text)
         if not found_contact_dict:
@@ -99,17 +130,52 @@ class Controller:
         view.print_msg(text_ru.CONTACTS_FOUND)
         view.print_phonebook(found_contact_dict)
 
+    def change_user(self) -> bool:
+        """
+        Изменение пользователя
+        :return: true в случае успешного изменения, иначе None
+        """
+        try:
+            id_to_delete = int(view.user_input(text_ru.CONTACT_CHANGE_ID))
+            view.print_phonebook({id_to_delete: self.phonebook.contacts[id_to_delete]})
+            field = view.user_input(text_ru.CONTACT_CHANGE_FIELDS)
+            if not field:
+                return None
+            elif field not in ('1', '2', '3', '4'):
+                raise FieldNotFound
+            new_value = view.user_input(text_ru.PRINT_NEW_VALUE)
+            field_key = text_ru.keys_dict[int(field)]
+            self.phonebook.change_contact(id_to_delete, field_key, new_value)
+        except ValueError:
+            view.print_msg(text_ru.CONTACT_CHANGE_INCORRECT_ID)
+        except (KeyError, model.IdNotFound):
+            view.print_msg(text_ru.CONTACT_NOT_FOUND)
+        except FieldNotFound:
+            view.print_msg(text_ru.FIELD_NOT_FOUND)
+        else:
+            view.print_msg(text_ru.CONTACT_CHANGED_SUCCESSFULLY)
+            view.print_phonebook({id_to_delete: self.phonebook.contacts[id_to_delete]})
+            return True
 
 
 def _menu_validation(choice: str, len_menu: int) -> None:
+    """
+    Проверка выбранного пользователем пункта меню
+    :param choice: выбранный пользователем пунтк меню
+    :param len_menu: количество пунктов в меню
+    :return: none
+    """
     try:
-        model.validate_user_input(choice, len_menu)
-    except model.MenuIsNotValid:
+        if not choice.isdecimal() or 0 >= int(choice) or int(choice) > len_menu:
+            raise MenuIsNotValid
+    except MenuIsNotValid:
         view.print_msg(text_ru.MENU_IS_NOT_VALID)
 
 
 def start():
-    """Основная функция работы с меню"""
+    """
+    Основная функция работы с меню
+    """
     controller = Controller()
     file = model.FileWork()
     choice = '0'
@@ -119,7 +185,7 @@ def start():
         _menu_validation(choice, len(text_ru.MENU))
         if choice == '1':
             controller.open_file(file)
-        if not controller.is_file_opened and choice != '8':
+        if not controller.is_file_opened and choice in (str(i) for i in range(1, 7)):
             view.print_msg(text_ru.FILE_IS_NOT_OPEN)
         elif choice == '2':
             controller.write_to_file(file)
@@ -132,7 +198,8 @@ def start():
         elif choice == '5':
             controller.find_contact()
         elif choice == '6':
-            pass
+            if controller.change_user():
+                controller.is_info_saved = False
         elif choice == '7':
             res = controller.delete_contact()
             if res:
--- homework_02/model.py
+++ homework_02/model.py
@@ -1,44 +1,9 @@
 import json
 import os
-from collections import namedtuple
-
-
-class MenuIsNotValid(Exception):
-    pass
-
-
-def validate_user_input(user_choice, len_menu):
-    if not user_choice.isdecimal() or 0 >= int(user_choice) or int(user_choice) > len_menu:
-        raise MenuIsNotValid
 
 
 class FileIsNotJson(Exception):
     pass
-
-
-class FileWork:
-    def __init__(self, path='contacts.json'):
-        self.path = path
-
-    def write_to_file(self, contacts):
-        with open(self.path, "w", encoding="utf-8") as file:
-            file.write(json.dumps(contacts))
-
-    def read_from_file(self):
-        with open(self.path, "r") as my_file:
-            contacts_json = my_file.read()
-        contacts = json.loads(contacts_json)
-        return contacts["users"]
-
-    @staticmethod
-    def check_chosen_file(chosen_file : str) -> bool:
-        '''Проверка файла на существование '''
-        if not os.path.isfile(chosen_file):
-            raise FileNotFoundError
-        else:
-            if chosen_file[-5:] != '.json':
-                raise FileIsNotJson
-
 
 class ContactFieldsAreEmpty(Exception):
     pass
@@ -48,12 +13,54 @@ class IdNotFound(Exception):
     pass
 
 
+class FileWork:
+    def __init__(self, path='contacts.json'):
+        self.path = path
+
+    def write_to_file(self, contacts: dict[str, list[dict[str, str]]]) -> None:
+        """
+
+        :param contacts: словарь: ключ users значение - список словарей контактов
+        :return: none
+        """
+        with open(self.path, "w", encoding="utf-8") as file:
+            file.write(json.dumps(contacts))
+
+    def read_from_file(self) -> list[dict[str, str]]:
+        """
+        Чтение данных из файла и их возврат в виде списка словарей
+        :return: список словарей контактов
+        """
+        with open(self.path, "r") as my_file:
+            contacts_json = my_file.read()
+        contacts = json.loads(contacts_json)
+        return contacts["users"]
+
+    @staticmethod
+    def check_chosen_file(chosen_file: str) -> None:
+        """
+        Проверка файла на существование и корректность формата
+        :param chosen_file: путь к фаайлу
+        :return: none
+        """
+        if not os.path.isfile(chosen_file):
+            raise FileNotFoundError
+        else:
+            if chosen_file[-5:] != '.json':
+                raise FileIsNotJson
+
+
 class PhoneBook:
 
     def __init__(self, contacts: dict[int, dict[str, str]] = {}) ->dict[int, dict[str, str]]:
         self.contacts = contacts
 
-    def add_contact(self, contact: dict[str, str]):
+    def add_contact(self, contact: dict[str, str]) -> dict[int, dict[str, str]]:
+        """
+        Добавление нового контакта записную книгу
+        :param contact: словарь с ключами и значениями для добавления нового контакта
+        :return: словарь словарей, где ключ - айди нового контакта, словарь с информацией по контакту
+        """
         if not all(list(map(lambda x: x != '', contact.values()))):
             raise ContactFieldsAreEmpty
         new_id = max(self.contacts.keys())+1 if self.contacts else 1
@@ -61,15 +68,30 @@ class PhoneBook:
         return {new_id: self.contacts[new_id]}
 
     def _is_id_in_dict(self, id_contact: int) -> bool:
+        """
+        Проверка наличия айди в списке контактов
+        :param id_contact: айди для поиска
+        :return: true если находится, исключение иначе
+        """
         if int(id_contact) not in self.contacts.keys():
             raise IdNotFound
         return True
 
     def delete_contact_by_id(self, id_contact: int) -> None:
+        """
+        Удаление контакта из телефонной книги по айди
+        :param id_contact: айди контакта для удаления
+        :return: none
+        """
         if self._is_id_in_dict(id_contact):
             del self.contacts[id_contact]
 
     def find_contact(self, text: str) -> [dict[int, dict[str, str]]]:
+        """
+        Поиск контактов, в значениях полей которых есть вхождение выбранной строки
+        :param text: строка для поиска
+        :return: список найденных контактов (список словарей, где словарь - ключи значения отдельного контакта)
+        """
         res = {}
         for key, value in self.contacts.items():
             for key1, value1 in value.items():
@@ -77,4 +99,16 @@ class PhoneBook:
                     res[key] = value
                     break
         return res
+
+    def change_contact(self, contact_id: int,  field_key: str, field_value: str) -> None:
+        """
+        Изменение контакта по айди
+        :param contact_id: айди контакта для изменения
+        :param field_key: ключ изменяемого поля
+        :param field_value: новое значение
+        :return:
+        """
+        if not self._is_id_in_dict(contact_id):
+            raise IdNotFound
+        self.contacts[contact_id][field_key] = field_value
 
