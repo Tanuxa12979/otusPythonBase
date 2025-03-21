cbcd89e77bd19f36c44446ff0a89117ab928aedd
Tatiana Zhigulina <zhigulinats@yandex.ru>
2025-03-21 14:38:50 +0300

    Before changes

--- homework_04/main.py
+++ homework_04/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from homework_04.routers.user import router  # Импортируем маршруты пользователей
 from database import engine, Base
-import uvicorn
 
 # Создаем экземпляр FastAPI
 app = FastAPI()
@@ -15,8 +14,8 @@ app.include_router(router, prefix="/user", tags=["users"])
 # Запуск приложения
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
-    app.config['DEBUG'] = False
-
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 
