684494d8548888d98a9c6b403152bd18e53e7160
Tatiana Zhigulina <zhigulinats@yandex.ru>
2025-04-07 22:07:50 +0300

    little changes

--- store_app/models.py
+++ store_app/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='comments')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
 
     def __str__(self):
