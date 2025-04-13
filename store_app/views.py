78f101f0eec6668c6601de6536c08a312ffb82f3
Tatiana Zhigulina <zhigulinats@yandex.ru>
2025-04-13 17:42:24 +0300

    added product page

--- store_app/views.py
+++ store_app/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "store_app/products_list.html", context=context)
 
 
-def about(request):
-    return HttpResponse("About")
+def product(request, id: int):
+    #product = Product.objects.get(id=id)
+    product = get_object_or_404(Product, pk=id)
+    context = {"product": product}
+    return render(request, "store_app/product.html", context=context)
